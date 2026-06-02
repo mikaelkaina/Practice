a9e0199e66d8c871ad085427640e32a414f412b1
Mikael Santos <mykaelsantos@outlook.com.br>
2026-06-01 22:19:07 -0300

    testes concluidos

---
+++ tests/DomainTests/UserBuilder.cs
@@ -0,0 +1,49 @@
+﻿using Bogus;
+using Practice.Domain;
+
+namespace DomainTests;
+
+public class TestUser : User
+{
+    public TestUser() : base() { }
+    public TestUser(Guid id, string firstName, string lastName, int age) : base(id, firstName, lastName, age) { }
+}
+public class UserBuilder
+{
+    private Guid _id;
+    private string _firstName;
+    private string _lastName;
+    private int _age;
+
+    public UserBuilder()
+    {
+        var faker = new Faker();
+        _id = Guid.NewGuid();
+        _firstName = faker.Name.FirstName();
+        _lastName = faker.Name.LastName();
+        _age = faker.Random.Int(0, 100);
+    }
+
+    public UserBuilder WithFirstName(string firstName)
+    {
+        _firstName = firstName;
+        return this;
+    }
+
+    public UserBuilder WithLastName(string lastName)
+    {
+        _lastName = lastName;
+        return this;
+    }
+
+    public UserBuilder WithAge(int age)
+    {
+        _age = age;
+        return this;
+    }
+
+    public Action BuildValidateAction()
+    {
+        return () => new TestUser().Validate(_id, _firstName, _lastName, _age);
+    }
+}
---
+++ tests/DomainTests/UserTests.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using Practice.Domain;
+
+namespace DomainTests;
+
+public class UserTests
+{
+    [Fact]
+    public void Validate_WithValidData_ShouldNotThrowException()
+    {
+        var validateAction = new UserBuilder().BuildValidateAction();
+        validateAction.Should().NotThrow();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public void Validate_WithInvalidFirstName_ShouldThrowDomainException(string? invalidFirstName)
+    {
+        var validateAction = new UserBuilder()
+            .WithFirstName(invalidFirstName!)
+            .BuildValidateAction();
+
+        validateAction.Should()
+            .Throw<DomainException>()
+            .WithMessage("First name is required.");
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public void Validate_WithInvalidLastName_ShouldThrowDomainException(string? invalidLastName)
+    {
+        var validateAction = new UserBuilder()
+            .WithLastName(invalidLastName!)
+            .BuildValidateAction();
+
+        validateAction.Should()
+            .Throw<DomainException>()
+            .WithMessage("Last name is required.");
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(101)]
+    public void Validate_WithInvalidAge_ShouldThrowDomainException(int invalidAge)
+    {
+        var validateAction = new UserBuilder()
+            .WithAge(invalidAge)
+            .BuildValidateAction();
+
+        validateAction.Should()
+            .Throw<DomainException>()
+            .WithMessage("Age must be between 0 and 100.");
+    }
+}
